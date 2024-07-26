102ece1209da92c37408f9eb490531708623728e
Deetsy <amardeepdeepak2016@gmail.com>
2024-07-26 19:25:42 +0530

    add index.html to server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var http = require("http");
 var url = require("url");
+var fs = require("fs");
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
+    fs.readFile("files/index.html", (err, data) => {
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   })
   .listen(8080);
