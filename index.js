c63052243cb0bece1f3de475a12fe3b37c93486f
Deetsy <amardeepdeepak2016@gmail.com>
2024-07-27 14:46:39 +0530

    finish building first website still some error

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ var url = require("url");
 var fs = require("fs");
 http
   .createServer(function (req, res) {
-    fs.readFile("files/index.html", (err, data) => {
-      res.writeHead(200, { "Content-type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+    let q = url.parse(req.url, true);
+    console.log(q.pathname);
+    if (q.pathname == "/") {
+      fs.readFile("files/index.html", (err, data) => {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else if (
+      q.pathname === "/index" ||
+      q.pathname === "/about" ||
+      q.pathname === "/contactme"
+    ) {
+      var filename = "files" + q.pathname + ".html";
+      fs.readFile(filename, (err, data) => {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile("files/404.html", (err, data) => {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
